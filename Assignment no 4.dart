a0c05cd771e91204310f8dc2a9b0f1a054ccac81
Muhammad Adnan <79781004+Adnan-Swati@users.noreply.github.com>
2021-10-02 11:05:10 -0700

    Add files via upload

---
+++ Assignment no 4.dart
@@ -0,0 +1,135 @@
+// Answer no 2
+
+
+// void main(){
+ 
+  //   table(2);
+ 
+// }
+
+// dynamic table(var a){
+//   for(var i=0; i<=10; i++){
+//     print ('${i} * ${a} = ${i * a} ');
+//   }
+// }
+
+// Answer No 3
+
+// void main(){
+  
+  
+//   var b2 = [2,4,6,8,9];
+  
+//   for(var i in b2){
+//     abc1 (i);
+//   }
+// }
+
+// dynamic abc1(var b1){
+  
+// for( var a=2; a<=2; a++){
+//   print ('$b1 / $a = ${b1 / a}');
+// }
+  
+// }
+   
+// Answwer No 04
+  
+//  void main(){
+   
+   
+// marksheet();
+   
+//    }            
+             
+// dynamic marksheet(){
+  
+//   var names = ['Adnan','Zeeshan','Sheraz','Yasir','Hasnain'];
+//     var marks = [85,95,83,79,88];
+//   var map1 = Map.fromIterables(names,marks);
+  
+//   for(MapEntry i in map1.entries){
+//     print ("${i.key},${i.value}");
+//   }
+   
+// }
+
+// Answer No 05
+
+
+// void main(){
+//   var obj = class1();
+//   obj.fun1();
+// }
+
+// class class1{
+  
+//   dynamic fun1(){
+//     print ('Outside class');
+//   }
+  
+// }
+
+// Answer No 06
+
+// void main(){
+//   var obj = name();
+//   obj.name1();
+// }
+
+// class name{
+//   dynamic name1(){
+//      print ('My name is Muhammad Adnan');
+//   }
+ 
+// }
+
+// Answer No 07
+
+// void main(){
+//   abc1();
+// }
+ 
+// class abc1{
+//    abc1(){
+//      var a =["Name","Age","Education"];
+//      var b =["adnan",19,'BSCS'];
+//      var c =Map.fromIterables(a,b);
+//      print (c);
+//    }
+// }
+
+
+// Answer No 08
+
+// void main(){
+//  vehicle obj = vehicle();
+//   obj.engine();
+//   bike obj1 = bike();
+//   obj1.body();
+//   cycle obj3 = cycle();
+//   obj3.colour();
+//  bike().engine();
+//  cycle().body();
+// }
+
+// class vehicle 
+// {
+//   dynamic engine(){
+//     print ("Engine");
+//   }
+// }
+
+// class bike extends vehicle
+// {
+//   dynamic body(){
+//     print ('Body');
+//   }
+// }
+
+// class cycle extends bike
+// {
+//   dynamic colour(){
+//     print ('Colour');
+//   }
+// }
